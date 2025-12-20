f8594e83ed5c2ca12174e76b9365bc2a3c6991b5
João Victor <joaovictorbc1457@gmail.com>
2025-12-20 13:08:40 -0300

    Arrumei a porta do servidor

--- server.js
+++ server.js
@@ -362,4 +362,5 @@ function broadcastState() {
     io.emit('game_update', payload); 
 }
 
-server.listen(3000, () => console.log('Servidor ON 3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
